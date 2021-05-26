02e412607eac2f69274a55e45df7618e86ed373b
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-05-26 08:42:04 +0530

    Cart and order placing

--- app/src/main/java/com/projectfire/bookmybook/ui/activities/SettingsActivity.kt
+++ app/src/main/java/com/projectfire/bookmybook/ui/activities/SettingsActivity.kt
@@ -53,7 +53,7 @@ class SettingsActivity : BaseActivity(), View.OnClickListener {
         GlideLoader(this@SettingsActivity).loadUserPicture(user.image, iv_user_photo)
         tv_name.text = "${user.firstName} ${user.lastName}"
         tv_email.text = user.email
-        tv_gender.text = user.gender
+        //tv_gender.text = user.gender
         tv_mobile_number.text = "${user.mobile}"
     }
 
